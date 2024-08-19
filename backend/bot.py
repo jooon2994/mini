ea3c6681feee4b891520a3035850db8bf67272e9
Yohannis Aweke <99585572+jooon2994@users.noreply.github.com>
2024-08-19 12:11:00 +0300

    ui

--- backend/bot.py
+++ backend/bot.py
@@ -1,31 +1,18 @@
-import telebot
-import logging
+from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup
+from telegram.ext import Updater, CommandHandler, CallbackContext
 
-# Enable logging
-logging.basicConfig(level=logging.INFO)
+def start(update: Update, context: CallbackContext):
+    keyboard = [[InlineKeyboardButton("Open Shop", url="https://mini-beta-six.vercel.app/")]]
+    reply_markup = InlineKeyboardMarkup(keyboard)
+    update.message.reply_text("Welcome To Ahadu Market Online Shopping! Click the button below to open the shop:", reply_markup=reply_markup)
 
-TOKEN = '7289662775:AAEeOB-MiJNYJSToqr3TATuAVtY-N9f0Ke4'
-bot = telebot.TeleBot(TOKEN)
-
-@bot.message_handler(commands=['start', 'help'])
-def send_welcome(message):
-    try:
-        print(f"Received command: {message.text}")
-        bot.reply_to(message, "Welcome to the Mini Shopping App!")
-    except Exception as e:
-        print(f"Error: {e}")
-
-@bot.message_handler(func=lambda message: True)
-def echo_all(message):
-    try:
-        print(f"Received message: {message.text}")
-        bot.reply_to(message, "This is a response from your bot!")
-    except Exception as e:
-        print(f"Error: {e}")
+def main():
+    updater = Updater("7289662775:AAEeOB-MiJNYJSToqr3TATuAVtY-N9f0Ke4", use_context=True)
+    updater.dispatcher.add_handler(CommandHandler("start", start))
+    
+    # Start the bot
+    updater.start_polling()
+    updater.idle()
 
 if __name__ == '__main__':
-    try:
-        print("Bot is running...")
-        bot.polling(none_stop=True)
-    except Exception as e:
-        print(f"Error while polling: {e}")
+    main()
